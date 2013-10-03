5b02d3f0a9f931083e6f263c9ef4067342188aeb
Clinton Gormley <clint@traveljury.com>
2013-10-03 18:53:39 +0200

    Added travis testing

--- cpanfile
+++ cpanfile
@@ -31,5 +31,7 @@ on 'configure' => sub {
 };
 
 on 'develop' => sub {
+  requires "Test::More" => "0";
+  requires "Test::NoTabs" => "0";
   requires "Test::Pod" => "1.41";
 };
---
+++ run_travis_tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -z $ES_VERSION ]; then
+    echo "No ES_VERSION specified";
+    exit 1;
+fi;
+
+ES_DIR="elasticsearch-$ES_VERSION"
+
+killall java 2>/dev/null
+
+if [ ! -d $ES_DIR ]; then
+    echo "Downloading Elasticsearch v${ES_VERSION}"
+    ES_URL="https://download.elasticsearch.org/elasticsearch/elasticsearch/${ES_DIR}.zip"
+    curl -O $ES_URL
+    unzip "${ES_DIR}.zip"
+fi;
+
+git clone https://github.com/elasticsearch/elasticsearch-perl.git
+
+ES_HOME=./$ES_DIR prove -I elasticsearch-perl/lib -l t/*.t
+RESULT=$?
+killall java 2>/dev/null
+exit $RESULT
