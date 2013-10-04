b73c9d17f98745a51d2b6e15c857df6d0fca2611
Clinton Gormley <clint@traveljury.com>
2013-10-04 10:35:18 +0200

    Output log after testing with travis

--- run_travis_tests.sh
+++ run_travis_tests.sh
@@ -20,5 +20,6 @@ git clone https://github.com/elasticsearch/elasticsearch-perl.git
 
 ES_HOME=./$ES_DIR prove -I elasticsearch-perl/lib -l -v t/*.t
 RESULT=$?
+cat log
 killall java 2>/dev/null
 exit $RESULT
